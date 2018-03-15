855a607860b4b772a9bb2796c85a9c0fb02fe8c2
Sergey Ladutko <verus774@mail.ru>
2018-03-15 20:15:01 +0300

    fix: use backtracking

--- src/index.js
+++ src/index.js
@@ -1,74 +1,77 @@
 module.exports = function solveSudoku(matrix) {
-    const values = mapPossibleValues(matrix);
-    const matrixCopy = [...matrix];
+    let i, row, col, value, found;
+    const zeroPositions = getZeroPositions(matrix);
 
-    if (values) {
-        for (let obj of values) {
-            let {row, col, values} = obj;
-            for (let v of values) {
-                matrixCopy[row][col] = v;
-                const result = solveSudoku(matrixCopy);
-                if (result) {
-                    return result;
-                }
+    for (i = 0; i < zeroPositions.length;) {
+        row = zeroPositions[i][0];
+        col = zeroPositions[i][1];
+        value = matrix[row][col] + 1;
+        found = false;
+
+        while (!found && value <= 9) {
+            if (checkValue(matrix, col, row, value)) {
+                found = true;
+                matrix[row][col] = value;
+                i++;
+            }
+            else {
+                value++;
             }
         }
+        if (!found) {
+            matrix[row][col] = 0;
+            i--;
+        }
     }
+
     return matrix;
 };
 
-function diff(a, b) {
-    return a.filter(x => b.indexOf(x) < 0);
-}
-
-function getColumnValues(columnIndex, matrix) {
-    const res = [];
+function checkRow(matrix, row, value) {
     for (let i = 0; i < 9; i++) {
-        res.push(matrix[i][columnIndex]);
+        if (matrix[row][i] === value) {
+            return false;
+        }
     }
-    return res;
+    return true;
 }
 
-function getRowValues(rowIndex, matrix) {
-    return matrix[rowIndex];
+function checkcol(matrix, col, value) {
+    for (let i = 0; i < 9; i++) {
+        if (matrix[i][col] === value) {
+            return false;
+        }
+    }
+    return true;
 }
 
-function getBlockValues(rowIndex, columnIndex, matrix) {
-    const res = [];
-
-    const blockRowStart = 3 * (Math.floor(rowIndex / 3));
-    const blockColumnStart = 3 * (Math.floor(columnIndex / 3));
+function checkBlock(matrix, col, row, value) {
+    const blockRowStart = 3 * (Math.floor(row / 3));
+    const blockCcolStart = 3 * (Math.floor(col / 3));
 
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-            res.push(matrix[blockRowStart + i][blockColumnStart + j]);
+            if (matrix[blockRowStart + i][blockCcolStart + j] === value) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function checkValue(matrix, col, row, value) {
+    return checkRow(matrix, row, value) &&
+        checkcol(matrix, col, value) &&
+        checkBlock(matrix, col, row, value);
+}
+
+function getZeroPositions(matrix) {
+    const res = [];
+
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            if (matrix[i][j] === 0) res.push([i, j]);
         }
     }
     return res;
-}
-
-function getPossibleValues(rowIndex, columnIndex, matrix) {
-    let values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    values = diff(values, getRowValues(rowIndex, matrix));
-    values = diff(values, getColumnValues(columnIndex, matrix));
-    values = diff(values, getBlockValues(rowIndex, columnIndex, matrix));
-
-    return values;
-}
-
-function mapPossibleValues(matrix) {
-    const res = [];
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            if (matrix[i][j] !== 0) continue;
-
-            const values = getPossibleValues(i, j, matrix);
-            if (values.length === 0) return false;
-            res.push({row: i, col: j, values});
-        }
-    }
-
-    const VALUES_COMPARATOR = (left, right) => left.values.length - right.values.length;
-    return res.sort(VALUES_COMPARATOR);
 }
